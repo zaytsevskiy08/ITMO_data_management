e9f42245621bb5a9fcac30a55fbf8c243af34cf5
zaytsevskiy08 <129918151+zaytsevskiy08@users.noreply.github.com>
2023-11-25 21:57:20 +0300

    Update src_to_docs.mjs

--- src_to_docs.mjs
+++ src_to_docs.mjs
@@ -1,9 +1,5 @@
 import { run } from "@mermaid-js/mermaid-cli"
-import puppeteer from "puppeteer"
 
-const browser = await puppeteer.launch({
-  headless: "new"
-})
 
 await run("src/Components.mmd", "docs/Components.svg")
 await run("src/Containers.mmd", "docs/Containers.svg")
