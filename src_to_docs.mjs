b3595123f0911fb9f72f2ebdcc88e2c3b764df76
zaytsevskiy08 <129918151+zaytsevskiy08@users.noreply.github.com>
2023-11-25 21:35:17 +0300

    Update src_to_docs.mjs

--- src_to_docs.mjs
+++ src_to_docs.mjs
@@ -1,16 +1,4 @@
 import { run } from "@mermaid-js/mermaid-cli"
-import puppeteer from "puppeteer-core" // добавьте импорт Puppeteer
-
-const generateSVG = async (input, output) => {
-  const browser = await puppeteer.launch({ headless: "new" });
-  
-  try {
-    await run(input, output);
-  } finally {
-    await browser.close();
-  }
-};
-
 
 await run("src/Components.mmd", "docs/Components.svg")
 await run("src/Containers.mmd", "docs/Containers.svg")
